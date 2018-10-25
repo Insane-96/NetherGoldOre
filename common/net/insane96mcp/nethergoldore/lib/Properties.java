d0462f0e730a5c7c291057c4ff9fa3b18792df2a
Insane-96 <insane96mcp@gmail.com>
2018-10-25 18:12:11 +0200

    1.2.0 - Fixed config names

--- common/net/insane96mcp/nethergoldore/lib/Properties.java
+++ common/net/insane96mcp/nethergoldore/lib/Properties.java
@@ -8,6 +8,7 @@ import net.minecraftforge.common.config.Config.Type;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = NetherGoldOre.MOD_ID, name = "NetherGoldOre", category = "")
 public class Properties {
@@ -20,36 +21,47 @@ public class Properties {
 		public Drops drops = new Drops();
 		
 		public static class Drops {
+			@Name("Minimum Nuggets Per Ore")
 			@Comment("Minumum nuggets drop from the Ore")
 			public int minNuggetsPerOre = 4;
+			@Name("Maximum Nuggets Per Ore")
 			@Comment("Maximum nuggets drop from the Ore (without fortune). Every Level of fortune increases this value by 1")
 			public int maxNuggetsPerOre = 6;
-			
+
+			@Name("Minimum Experience Per Ore")
 			@Comment("Minumum amount of experience drop from the Ore")
 			public int minExperienceDrop = 0;
+			@Name("Maximum Experience Per Ore")
 			@Comment("Maximum amount of experience drop from the Ore")
 			public int maxExperienceDrop = 2;
 		}
 		
-		@Name("properties")
+		@Name("ore properties")
 		public OreProperties oreProperties = new OreProperties();
 		
 		public static class OreProperties {
+			@Name("Pigman Aggro Chance")
 			@Comment("Chance for zombie pigman to get aggroed when a nether gold ore is mined. Set to 0 to disable")
 			public float pigmanAggroChance = 5f;
+			@Name("Pigman Aggro Radius")
 			@Comment("Radius (from ore mined) for zombie pigman to get aggroed when a nether gold ore is mined\nThe radius has to be intended as a cube and not a sphere")
 			public float pigmanAggroRadius = 24f;
 		}
 		
+		@Name("ore generation")
 		public Generation generation = new Generation();
 		
 		public static class Generation {
+			@Name("Ore Per Vein")
 			@Comment("Number of blocks generated per vein (not exact)")
 			public int orePerVein = 22;
+			@Name("Vein Per Chunk")
 			@Comment("Number of veins that have to try to spawn per chunk, values below 1 will be chance per chunk to spawn. (By default there's 80% chance for a vein to spawn per chunk)")
 			public float veinPerChunk = 0.8f;
+			@Name("Min Y")
 			@Comment("The minimum height (Y) to try to generate Veins")
 			public int minY = 0;
+			@Name("Max Y")
 			@Comment("The maximum height (Y) to try to generate Veins")
 			public int maxY = 128;
 		}
@@ -57,6 +69,7 @@ public class Properties {
 
 	@Mod.EventBusSubscriber(modid = NetherGoldOre.MOD_ID)
 	private static class EventHandler{
+		@SubscribeEvent
 	    public static void onConfigChangedEvent(OnConfigChangedEvent event)
 	    {
 	        if (event.getModID().equals(NetherGoldOre.MOD_ID))
--- common/net/insane96mcp/nethergoldore/proxies/CommonProxy.java
+++ common/net/insane96mcp/nethergoldore/proxies/CommonProxy.java
@@ -1,11 +1,6 @@
 package net.insane96mcp.nethergoldore.proxies;
 
-import net.insane96mcp.nethergoldore.NetherGoldOre;
 import net.insane96mcp.nethergoldore.init.ModBlocks;
-import net.insane96mcp.nethergoldore.lib.Properties;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Config.Type;
-import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -13,14 +8,10 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class CommonProxy {
 	public void PreInit(FMLPreInitializationEvent event) {
 		ModBlocks.Init();
-		
-		MinecraftForge.EVENT_BUS.register(Properties.class);
 	}
 	
 	public void Init(FMLInitializationEvent event) {
 		ModBlocks.PostInit();
-		
-		ConfigManager.sync(NetherGoldOre.MOD_ID, Type.INSTANCE);
 	}
 	
 	public void PostInit(FMLPostInitializationEvent event) {
